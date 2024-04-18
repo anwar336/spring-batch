57b99752a389e8485837bb5b7dbe453531a9a46d
Md. Anwarul Islam <anwarul.islam@konasl.com>
2024-04-18 17:42:56 +0600

    update

--- src/main/java/com/kit/migrator/datamigrator/gateway/model/MisRequestModel.java
+++ src/main/java/com/kit/migrator/datamigrator/gateway/model/MisRequestModel.java
@@ -114,6 +114,11 @@ public class MisRequestModel {
 
         this.legal_status = dto.getRespondentLegalStatus() != null ? dto.getRespondentLegalStatus().name() : null;
         this.selection_criteria = dto.getSelectionCriteria() != null ? dto.getSelectionCriteria().name() : null;
-        this.selection_reason = dto.getSelectionReason() != null ? dto.getSelectionReason().get(0).name() : null;
+        if(dto.getSelectionReason() != null && dto.getSelectionReason().size() > 0){
+            if(dto.getSelectionReason().get(0) != null){
+                this.selection_reason = dto.getSelectionReason().get(0).name();
+            }
+        }
+
     }
 }
