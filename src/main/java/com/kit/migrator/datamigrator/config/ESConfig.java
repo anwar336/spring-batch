19bc16cb93c726eb9af14b29313c080b3a235b33
anwar336 <anwar336@gmail.com>
2024-04-28 16:52:04 +0600

    s

--- src/main/java/com/kit/migrator/datamigrator/config/ESConfig.java
+++ src/main/java/com/kit/migrator/datamigrator/config/ESConfig.java
@@ -5,36 +5,36 @@
  */
 package com.kit.migrator.datamigrator.config;
 
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.message.BasicHeader;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Base64;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.xcontent.XContentType;
+//import org.apache.http.Header;
+//import org.apache.http.HttpHost;
+//import org.apache.http.message.BasicHeader;
+//import org.elasticsearch.client.RestClient;
+//import org.elasticsearch.client.RestClientBuilder;
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Base64;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.http.auth.AuthScope;
+//import org.apache.http.auth.UsernamePasswordCredentials;
+//import org.apache.http.client.CredentialsProvider;
+//import org.apache.http.impl.client.BasicCredentialsProvider;
+//import org.elasticsearch.client.RequestOptions;
+//import org.elasticsearch.client.indices.CreateIndexRequest;
+//import org.elasticsearch.client.indices.CreateIndexResponse;
+//import org.elasticsearch.client.indices.GetIndexRequest;
+//import org.elasticsearch.xcontent.XContentType;
 
 /**
  *
  * @author USER
  */
 //@Configuration
-@Slf4j
+//@Slf4j
 public class ESConfig {
-
+/*
     @Value("${elastic.hosts}")
     private String hosts;
 
@@ -65,5 +65,5 @@ public class ESConfig {
                 .setDefaultCredentialsProvider(credentialsProvider));
         return new RestHighLevelClient(builder);
     }
-
+    */
 }
--- src/main/java/com/kit/migrator/datamigrator/service/BeneficiaryESWriter.java
+++ src/main/java/com/kit/migrator/datamigrator/service/BeneficiaryESWriter.java
@@ -6,19 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 
 import java.util.List;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.xcontent.XContentType;
+//import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+//import org.elasticsearch.action.index.IndexRequest;
+//import org.elasticsearch.action.index.IndexResponse;
+//import org.elasticsearch.client.RequestOptions;
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.elasticsearch.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 public class BeneficiaryESWriter implements ItemWriter<BeneficiaryDto> {
 
     //@Autowired
-    private RestHighLevelClient client;
+    //private RestHighLevelClient client;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -27,16 +27,16 @@ public class BeneficiaryESWriter implements ItemWriter<BeneficiaryDto> {
     public void write(List<? extends BeneficiaryDto> beneficiaryList) throws Exception {
         beneficiaryList.forEach(b -> {
             log.info(" >>> " + b.getApplicationId());
-            try {
-                IndexRequest indexRequest = new IndexRequest("index_beneficiary");
-                    indexRequest.id(b.getApplicationId());
-                indexRequest.source(this.objectMapper.writeValueAsString(b), XContentType.JSON);
-                IndexResponse response = this.client.index(indexRequest, RequestOptions.DEFAULT);
-                log.info("==INDEX RESULT:{}", response.status());
-                this.client.indices().refresh(new RefreshRequest("index_beneficiary"), RequestOptions.DEFAULT);
-            } catch (Throwable t) {
-                log.error("", t);
-            }
+//            try {
+//                IndexRequest indexRequest = new IndexRequest("index_beneficiary");
+//                    indexRequest.id(b.getApplicationId());
+//                indexRequest.source(this.objectMapper.writeValueAsString(b), XContentType.JSON);
+//                IndexResponse response = this.client.index(indexRequest, RequestOptions.DEFAULT);
+//                log.info("==INDEX RESULT:{}", response.status());
+//                this.client.indices().refresh(new RefreshRequest("index_beneficiary"), RequestOptions.DEFAULT);
+//            } catch (Throwable t) {
+//                log.error("", t);
+//            }
         });
 
     }
